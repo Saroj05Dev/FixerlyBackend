0c0103c50086940b443e94ab0624a8587fb5f39d
Saroj_Dev <sarojtech0005@gmail.com>
2025-11-03 19:02:21 +0530

    Merge Conflict Resolved

--- src/schema/Service_schema.js
+++ src/schema/Service_schema.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const serviceSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String },
-    createdAt: { type: Date, default: Date.now }
+    // createdAt: { type: Date, default: Date.now }
+}, {
+    timestamps: true
 });
 
 const Service = mongoose.model('Service', serviceSchema);
